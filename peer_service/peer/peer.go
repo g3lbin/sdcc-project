364187479ca4132bd814a0e5a014a9f87e709eef
cristiano-u08 <cristia_no98@live.it>
2021-08-13 19:24:59 +0200

    Aggiunto meccanismo di base per l'invio di messaggi ad intervalli temporali pseudo-casuali

--- peer_service/peer/peer.go
+++ peer_service/peer/peer.go
@@ -3,8 +3,10 @@ package main
 import (
 	"fmt"
 	"log"
+	"math/rand"
 	"net/rpc"
 	"os"
+	"time"
 
 	"app/lib"
 )
@@ -13,13 +15,22 @@ func main() {
 	var msg lib.Message
 	var res lib.Result
 
-	addr := "10.5.0.2:" + "4321" //address and port on which RPC server is listening
+	rand.Seed(time.Now().UnixNano())
+	min := 5
+	max := 120
+
+	name, err := os.Hostname()
+	if err != nil {
+		log.Fatal("Hostname error: ", err)
+	}
+
+	addr := "registry:" + "4321" //address and port on which RPC server is listening
 	// Try to connect to addr
-	client, err := rpc.Dial("tcp", addr)
+	cl, err := rpc.Dial("tcp", addr)
 	if err != nil {
 		log.Fatal("Error in dialing: ", err)
 	}
-	defer client.Close()
+	defer cl.Close()
 
 	if len(os.Args) < 2 {
 		fmt.Printf("No args passed in\n")
@@ -31,13 +42,18 @@ func main() {
 	// reply will store the RPC result
 	// Call remote procedure
 	log.Printf("Synchronous call to RPC server")
-	err = client.Call("Writer.Write_on_terminal", msg, &res)
+	err = cl.Call("Writer.Write_on_terminal", msg, &res)
 	if err != nil {
 		log.Fatal("Error in Writer.Write_on_terminal: ", err)
 	}
 
 	fmt.Printf("Writer.Write_on_terminal result: %d\n\n", res)
 
+	for i := 0; i < 10; i++ {
+		r := rand.Intn(max - min + 1) + min
+		log.Printf("hostname: %s, rand: %d", name, r)
+		time.Sleep(time.Duration(r) * time.Second)
+	}
 	// Asynchronous call
 	// divReply := new(arith.Quotient)
 	// log.Printf("Asynchronous call to RPC server")
